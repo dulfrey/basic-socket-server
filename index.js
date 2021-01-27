067c34c36eeadd97d81b100dc7ad5d605e29e100
dulfrey <wdhernandezh@unal.edu.co>
2021-01-26 22:41:41 -0500

    basic config

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('star server')
+const path = require('path')
+const express = require('express');
+const app = express();
+const SocketIO = require('socket.io');
+const { Socket } = require('dgram');
+
+app.set('port', process.env.PORT || 4000 );
+
+//static files 
+
+app.use(express.static  ( path.join ( __dirname , 'public'))    );
+
+const server = app.listen( app.get('port') , ()=>{
+    console.log('server listen on port :: ' ,  app.get('port'));
+})
+const io = SocketIO(server)
+
+// websocket
+
+io.on('connection' , () => {
+    console.log(' new connection')
+})
+
